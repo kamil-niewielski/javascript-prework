5d0a658214142563d8165483baa2b9b788e2f8ac
Kamil Niewielski <nevel1906.kn@gmail.com>
2026-02-27 13:58:46 +0000

    dodany ruch playera js/script.js

--- js/script.js
+++ js/script.js
@@ -16,7 +16,20 @@ if (randomNumber == '1') {
 } else if (randomNumber == '2') {
   computerMove = 'papier';
 } else if (randomNumber == '3') {
-    computerMove = 'nozyce'
+    computerMove = 'nozyce';
 }
 printMessage('Mój ruch: ' + computerMove);
 
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+    playerMove = 'papier';
+} else if (playerInput == '3') {
+    playerMove = 'nozyce';
+} else  {
+  playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove);
